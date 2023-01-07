12dbfe4c8f39505a5dc39447c1faa0c9a9d46f19
mehul425 <m.makwana450@gmail.com>
2023-01-07 14:11:36 +0530

    change header params

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ var slug = url.substring(url.indexOf("=") + 1, url.length);
 console.log(slug);
 try {
 var x = await youtube.getVideo(slug);
-res.status(200).setHeader("Access-Control-Allow-Origin", "*")
-.setHeader("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE")
-.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+res.status(200).set("Access-Control-Allow-Origin", "*")
+.set("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE")
+.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 .json(JSON.parse(CircularJSON.stringify(x)));
 } catch (e) {
 console.log(e);
